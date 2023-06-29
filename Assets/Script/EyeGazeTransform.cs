1af4aa4cd6bf2eff7ce5027587e4a5c8d0dc62a7
Jubliani <59886167+Jubliani@users.noreply.github.com>
2023-06-29 08:23:42 -0700

    added .gitignore

---
+++ Assets/Script/EyeGazeTransform.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ViveSR.anipal.Eye;
+
+public class EyeGazeTransform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject left;
+    public GameObject right;
+    public GameObject startButton;
+    public GameObject target;
+    public GameObject target1;
+    public GameObject target2;
+    public GameObject target3;
+    public GameObject target4;
+    public GameObject target5;
+    public GameObject instruction;
+    private ViveSR.anipal.Eye.EyeDataCol eyeDataCol;
+    public float timeRemaining = 5.0f;
+    public float timeRemainingInstruction = 51.0f;
+    public bool timerOn = false;
+    private Vector3 leftPos;
+    private Vector3 rightPos;
+    string lObjectName = "N/A";
+    string rObjectName = "N/A";
+    int phaseNum = 1; // 1 = startButton has not been pressed yet, 2 = startButton pressed and instructions on, 3 = instructions done and testing
+    int stage = 1; // the current dartboard level the user is on
+    Color target1Default;
+    Color target2Default;
+    Color target3Default;
+    Color target4Default;
+    Color target5Default;
+
+    void Start()
+    {
+        instruction = GameObject.Find("Instruction");
+        eyeDataCol = GetComponent<ViveSR.anipal.Eye.EyeDataCol>();
+        target1Default = target1.GetComponent<Renderer>().material.color;
+        target2Default = target2.GetComponent<Renderer>().material.color;
+        target3Default = target3.GetComponent<Renderer>().material.color;
+        target4Default = target4.GetComponent<Renderer>().material.color;
+        target5Default = target5.GetComponent<Renderer>().material.color;
+
+        startButton.SetActive(true);
+        instruction.SetActive(false);
+        target.SetActive(false);
+        phaseNum = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // first phase, with the user not yet viewing the start button
+        if (phaseNum == 1 && (eyeDataCol.lObjectName == "StartButton" || eyeDataCol.rObjectName == "StartButton"))
+        {
+            Debug.Log("Starting game...");
+            startButton.SetActive(false);
+            target.SetActive(false);
+            instruction.SetActive(true);
+            target1.GetComponent<Renderer>().material.color = Color.red;
+            phaseNum = 2;
+            timeRemainingInstruction = 51.0f;
+        }
+
+        // second phase, with the user reading the instructions
+        if (phaseNum == 2)
+        {
+            if (timeRemainingInstruction > 0)
+            {
+                timeRemainingInstruction -= Time.deltaTime;
+
+            }
+            else
+            {
+                instruction.SetActive(false);
+                target1.GetComponent<Renderer>().material.color = target1Default;
+                target2.GetComponent<Renderer>().material.color = target2Default;
+                target3.GetComponent<Renderer>().material.color = target3Default;
+                target4.GetComponent<Renderer>().material.color = target4Default;
+                target5.GetComponent<Renderer>().material.color = target5Default;
+                phaseNum = 3;
+            }
+        }
+
+        // third and final phase, with the user trying to look at the dartboard
+        if (phaseNum == 3)
+        {
+            if (eyeDataCol.worldPosL != new Vector3(0, 0, 0))
+            {
+                left.SetActive(true);
+                left.transform.position = eyeDataCol.worldPosL;
+            }
+            if (eyeDataCol.worldPosR != new Vector3(0, 0, 0))
+            {
+                right.SetActive(true);
+                right.transform.position = eyeDataCol.worldPosR;
+            }
+
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                //lObjectName = eyeDataCol.hitInfoL.collider.gameObject.name;
+                //rObjectName = eyeDataCol.hitInfoR.collider.gameObject.name
+                timeRemaining = 5.0f;
+                stage += 1;
+                Debug.Log("Current stage: " + stage);
+            }
+            int leftNum = -1;
+            int rightNum = -1;
+            if (eyeDataCol.lObjectName.Length > 6)
+            {
+                int.TryParse(eyeDataCol.lObjectName.Substring(6), out leftNum);
+            }
+            if (eyeDataCol.rObjectName.Length > 6)
+            {
+                int.TryParse(eyeDataCol.rObjectName.Substring(6), out rightNum);
+            }
+            //Debug.Log(Vector3.Distance(leftPos, eyeDataCol.worldPosL));
+            if (leftNum < stage || rightNum < stage)
+            {
+                timeRemaining = 5.0f;
+                //Debug.Log("reset");
+            }
+
+            //Debug.Log(timeRemaining);
+
+            //Debug.Log(leftPos);
+
+            //Debug.Log(eyeDataCol.worldPosL);
+        }
+    }
+}
---
+++ Assets/Script/SRanipal_GazeRaySampleDataCol.cs
@@ -0,0 +1,207 @@
+﻿//========= Copyright 2018, HTC Corporation. All rights reserved. ===========
+using System.IO;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+
+namespace ViveSR
+{
+    namespace anipal
+    {
+        namespace Eye
+        {
+            public class EyeTracking : MonoBehaviour
+            {
+                public int LengthOfRay = 25;
+                public string Condition = "Office";
+                [SerializeField] public GameObject CenterEye;
+                Camera camL, camR, camC;
+
+                //[SerializeField] private LineRenderer GazeRayRenderer;
+                private static EyeData_v2 eyeData = new EyeData_v2();
+                private bool eye_callback_registered = false;
+
+                // data to save
+                public Vector3 screenPosC;
+                public Vector3 screenPosL;
+                public Vector3 screenPosR;
+                public Vector3 worldPosC;
+                public Vector3 worldPosL;
+                public Vector3 worldPosR;
+                public Vector3 GazeDirectionCombined;
+                public Vector3 GazeOriginCombinedLocal;
+                private Vector3 GazeDirectionCombinedLocal;
+                private RaycastHit hitInfoC, hitInfoL, hitInfoR;
+                public Vector3 L_Origin;
+                public Vector3 R_Origin;
+                public Vector3 L_Direction;
+                public Vector3 R_Direction;
+                public string lObjectName = "N/A";
+                public string rObjectName = "N/A";
+                public string cObjectName = "N/A";
+                private StreamWriter sw;
+
+                public Camera camera;
+
+                private void Start()
+                {
+                    
+
+                    sw = File.AppendText(Condition + "_" + System.DateTime.Now.ToString("MM-dd-yyyy") + "_" + "GazeDataAll.txt");
+
+                    string textAll = "Trial" + ", " + "Time" + ", " + "Type" + ", " + "L Pixel X " + " , " + "L Pixel Y " + " , " + "L Pixel Z" +
+                    ", " + "L hit point in Z" + " , " + "L Looking At" + ", " + "L World X" + ", " + "L World Y" + ", " + "R Pixel X " + ", " + "R Pixel Y " + " , " + "R Pixel Z" +
+                    ", " + "R hit point in Z" + " , " + "R Looking At" + ", " + "R World X" + ", " + "R World Y" + ", " + "C Pixel X " + ", " + "C Pixel Y " + " , " + "C Pixel Z" +
+                    ", " + "C hit point in Z" + " , " + "C Looking At" + ", " + "C World X" + ", " + "C World Y" + ", " + "L Origin X " + ", " + "L Origin Y " + " , " + "L Origin Z" +
+                    ", " + "R Origin X " + " , " + "R Origin Y " + " , " + "R Origin Z" + "\n";
+                    sw.Write(textAll);
+
+                    
+
+                    if (!SRanipal_Eye_Framework.Instance.EnableEye)
+                    {
+                        enabled = false;
+                        return;
+                    }
+                    //Assert.IsNotNull(GazeRayRenderer);
+
+                    camC = CenterEye.GetComponent<Camera>();
+
+
+                }
+
+                private void Update()
+                {
+
+                    //if (trial < spawnScript.getTrialNumber()) {}
+                    GetGazeData();
+                    //GazeRayRenderer.SetPosition(0, camC.transform.position - camC.transform.up * 0.05f);
+                    //GazeRayRenderer.SetPosition(1, hitInfoC.point);
+
+                }
+
+
+                
+
+                private void GetGazeData()
+                {
+                    //await Task.Delay(TimeSpan.FromSeconds(0));
+
+
+                    if (SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING &&
+                        SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.NOT_SUPPORT) return;
+
+                    if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == true && eye_callback_registered == false)
+                    {
+                        SRanipal_Eye_v2.WrapperRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback));
+                        eye_callback_registered = true;
+                    }
+                    else if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == false && eye_callback_registered == true)
+                    {
+                        SRanipal_Eye_v2.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback));
+                        eye_callback_registered = false;
+                    }
+
+                    if (eye_callback_registered)
+                    {
+                        if (SRanipal_Eye_v2.GetGazeRay(GazeIndex.COMBINE, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal, eyeData))
+                        {
+                            SRanipal_Eye_v2.GetGazeRay(GazeIndex.LEFT, out L_Origin, out L_Direction, eyeData);
+                            SRanipal_Eye_v2.GetGazeRay(GazeIndex.RIGHT, out R_Origin, out R_Direction, eyeData);
+
+                        }
+                        else
+                        {
+                            return;
+                        }
+
+                    }
+
+                    else
+                    {
+                        if (SRanipal_Eye_v2.GetGazeRay(GazeIndex.COMBINE, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal))
+                        {
+                            SRanipal_Eye_v2.GetGazeRay(GazeIndex.LEFT, out L_Origin, out L_Direction);
+                            SRanipal_Eye_v2.GetGazeRay(GazeIndex.RIGHT, out R_Origin, out R_Direction);
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+
+                    
+
+                    camL = camC;
+                    camL.transform.position += new Vector3(-0.03f, 0, 0);
+                    Vector3 GazeDirectionTestL = camL.transform.TransformDirection(L_Direction);
+                    if (Physics.Raycast(camL.transform.position, GazeDirectionTestL, out hitInfoL, 1000))
+                    {
+                        lObjectName = hitInfoL.collider.gameObject.name;
+                        screenPosL = camL.WorldToScreenPoint(hitInfoL.point, Camera.MonoOrStereoscopicEye.Mono);
+                        worldPosL = hitInfoL.point;
+                        screenPosL.z = hitInfoL.point.z;
+                    }
+
+                    camR = camC;
+                    camR.transform.position += new Vector3(0.03f, 0, 0);
+                    Vector3 GazeDirectionTestR = camR.transform.TransformDirection(R_Direction);
+                    if (Physics.Raycast(camR.transform.position, GazeDirectionTestR, out hitInfoR, 1000))
+                    {
+                        rObjectName = hitInfoR.collider.gameObject.name;
+                        screenPosR = camR.WorldToScreenPoint(hitInfoR.point, Camera.MonoOrStereoscopicEye.Mono);
+                        worldPosR = hitInfoR.point;
+                        screenPosR.z = hitInfoR.point.z;
+                    }
+
+                    Vector3 GazeDirectionTestC = camC.transform.TransformDirection(GazeDirectionCombinedLocal);
+                    if (Physics.Raycast(camC.transform.position, GazeDirectionTestC, out hitInfoC, 1000))
+                    {
+                        cObjectName = hitInfoC.collider.gameObject.name;
+                        screenPosC = camC.WorldToScreenPoint(hitInfoC.point, Camera.MonoOrStereoscopicEye.Mono);
+                        worldPosC = hitInfoC.point;
+                        screenPosC.z = hitInfoC.point.z;
+                    }
+
+                    string textAll = System.DateTime.Now.Ticks.ToString()  + ", " + screenPosL.x + ", " + screenPosL.y + ", " + screenPosL.z +
+                    ", " + hitInfoL.distance + ", " + lObjectName + ", " + worldPosL.x + ", " + worldPosL.y + ", " + screenPosR.x + ", " + screenPosR.y + ", " + screenPosR.z +
+                    ", " + hitInfoR.distance + ", " + rObjectName + ", " + worldPosR.x + ", " + worldPosR.y + ", " + screenPosC.x + ", " + screenPosC.y + ", " + screenPosC.z +
+                    ", " + hitInfoC.distance + ", " + cObjectName + ", " + worldPosC.x + ", " + worldPosC.y + ", " + L_Origin.x + ", " + L_Origin.y + ", " + L_Origin.z +
+                     ", " + R_Origin.x + ", " + R_Origin.y + ", " + R_Origin.z + "\n";
+                    
+                    sw.Write(textAll);
+
+                    
+                }
+
+                private void OnDisable()
+                {
+                    Release();
+                }
+
+                void OnApplicationQuit()
+                {
+                    Release();
+                }
+
+                private void Release()
+                {
+                    if (eye_callback_registered == true)
+                    {
+                        SRanipal_Eye_v2.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback));
+                        eye_callback_registered = false;
+                    }
+                }
+
+                private static void EyeCallback(ref EyeData_v2 eye_data)
+                {
+                    eyeData = eye_data;
+
+
+
+                }
+            }
+        }
+    }
+}
